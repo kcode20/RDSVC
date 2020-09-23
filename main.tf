c6b1de73f6e73bff511fc08e660a9f65d12acc14
Khristian Brooks <kh.brooks02@gmail.com>
2020-09-23 00:21:20 -0400

    add route table for public subnet

--- main.tf
+++ main.tf
@@ -27,7 +27,8 @@ resource "aws_subnet" "public-subnet" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1b"
-  tags{
+
+  tags = {
     "Name" = "Web Public Subnet"
   }
 }
@@ -37,7 +38,8 @@ resource "aws_subnet" "public-subnet" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = ""
   availability_zone = "us-east-1c"
-  tags{
+
+  tags = {
     "Name" = "Database Private Subnet"
   }
 }
@@ -45,11 +47,31 @@ resource "aws_subnet" "public-subnet" {
 # Define the internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.default.id}"
-  tags {
+
+  tags = {
     Name = "VPC IGW"
   }
 }
 
+# Define the route table for the public subnet
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags = {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
 
 # Creates an EC2 instance
 resource "aws_instance" "rdsvc-ec2" {
