5d282661563f0ddcf230cb7cb684176567926948
Khristian Brooks <kh.brooks02@gmail.com>
2020-09-23 00:09:50 -0400

    add vpc, public & private subnet, internet gateway

--- main.tf
+++ main.tf
@@ -7,11 +7,51 @@ terraform {
   }
 }
 
+# Define AWS as our provider
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
+# Define a VPC
+resource "aws_vpc" "main"{
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    "Name" = "main-vpc"
+  }
+}
+
+# Define a public subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  tags{
+    "Name" = "Web Public Subnet"
+  }
+}
+
+# Define a private subnet
+resource "aws_subnet" "public-subnet" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = ""
+  availability_zone = "us-east-1c"
+  tags{
+    "Name" = "Database Private Subnet"
+  }
+}
+
+# Define the internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.default.id}"
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+
+# Creates an EC2 instance
 resource "aws_instance" "rdsvc-ec2" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
