b505db8b6f700271548a26b3ab7361ba93374d90
Khristian Brooks <kh.brooks02@gmail.com>
2020-10-06 17:05:01 -0400

    add VPC Endpoint, edit Lambda package

--- main.tf
+++ main.tf
@@ -296,3 +296,15 @@ resource "aws_lambda_function" "rdsvc-lambda-function" {
     Name = "rdsvc-lambda-function"
   }
 }
+
+# Create VPC Endpoint
+resource "aws_vpc_endpoint" "saints-xctf-s3-vpc-endpoint" {
+  service_name      = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
+
+
+  route_table_ids = [aws_route_table.web-public-rt.id]
+}
--- rdsvc-lambda/backup.sh
+++ rdsvc-lambda/backup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Input Variables
 MYSQL_HOST=$1
@@ -8,29 +8,17 @@ MYSQL_PASS=$4
 MYSQL_DB=$5
 S3_BUCKET=$6
 
-# Move into /tmp
-cd /tmp
-
-# make sure mysql is installed
-yum install mysql
+cp "./rdsvc-lambda/mysqldump" /tmp/mysqldump
+chmod 755 /tmp/mysqldump
 
 # Define file for mysqldump output
-file=$(date +%F).sql
+file=/tmp/$(date +%F).sql
 
 # Call mysqldump and save it in defined file
-mysqldump \
-  --host ${MYSQL_HOST} \
+/tmp/mysqldump \
+  --host "${MYSQL_HOST}" \
   --port ${MYSQL_PORT} \
   -u ${MYSQL_USER} \
   --password="${MYSQL_PASS}" \
   ${MYSQL_DB} > ${file}
 
-# Zip the mysql file and upload it to S3 delete the file locally
-if [ "${?}" -eq 0 ]; then
-  gzip ${file}
-  aws s3 cp ${file}.gz s3://${S3_BUCKET}
-  rm ${file}.gz
-else
-  echo "Error backing up mysql"
-  exit 255
-fi
